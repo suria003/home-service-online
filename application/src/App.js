1026a5a421f6541d1d0594d2e18fddd6bb246d33
suria003 <suryadr1111@gmail.com>
2024-12-14 22:31:47 +0530

    authendication page design

--- application/src/App.js
+++ application/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-function App() {
+// Authendicaiton Module
+import Register from "./Component/Authendication/Register";
+import Login from "./Component/Authendication/Login";
+
+// Route import
+import Template from "./Component/Template";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+
+      <Router>
+        <Routes>
+
+          {/* Authendication */}
+          <Route path="/register" element={<Register />}/>
+          <Route path="/login" element={<Login />}/>
+
+          <Route path="/" element={<Template />}/>
+        </Routes>
+      </Router>
+
+    </React.Fragment>
   );
 }
-
-export default App;
---
+++ application/src/Component/Template.jsx
@@ -0,0 +1,5 @@
+export default function Template() {
+    return(
+        <main className="bg-black h-screen w-full"></main>
+    );
+}
